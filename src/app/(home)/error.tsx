83556474df44442423961c16a8a0ec262282f175
geislerovamarie <geislerova.marie@gmail.com>
2024-06-02 01:37:35 +0200

    loading and error handling added

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+const ErrorMessage = () => {
+  return (
+    <div className="m-5">
+      <h2 className="header">Velocity Vault</h2>
+      <div className="error-container">
+        <h2 className="font-bold">Something went wrong.</h2>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorMessage
---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,12 @@
+const LoadingMessage = () => {
+  return (
+    <div className="m-5">
+      <h2 className="header">Velocity Vault</h2>
+      <div className="loading-container">
+        <h2 className="font-bold">Loading</h2>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingMessage
